923acab758c1158664ef2cb4c23d6493b86169fb
Naima Africa Francis <naima.africa@gmail.com>
2020-09-07 14:10:51 -0400

    Added Codewars code to file

--- Controllers/ExplosionController.cs
+++ Controllers/ExplosionController.cs
@@ -11,5 +11,54 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class ExplosionController : ControllerBase
     {
+        public class Digits
+        {
+            [HttpGet]
+            public static string Explode(string s)
+            {
+                var initialString = "";
+
+                for (var index = 0; index < s.Length; index++)
+                {
+                    var numberOfCharacter = s[index].ToString();
+
+                    var numbers = Int32.Parse(numberOfCharacter);
+
+                    for (var otherIndex = 0; otherIndex < numbers; otherIndex++)
+                    {
+                        initialString += numberOfCharacter;
+                    }
+                }
+                return initialString;
+            }
+        }
+        public class Accumul
+        {
+            [HttpGet]
+            public static String Accum(string input)
+            {
+                var answer = "";
+                for (var index = 0; index < input.Length; index++)
+                {
+                    var currentCharacter = input[index];
+                    for (var repeatCount = 0; repeatCount < index + 1; repeatCount++)
+                    {
+                        if (repeatCount == 0)
+                        {
+                            answer += Char.ToUpper(currentCharacter);
+                        }
+                        else
+                        {
+                            answer += Char.ToLower(currentCharacter);
+                        }
+                    }
+                    if (index < input.Length - 1)
+                    {
+                        answer += "-";
+                    }
+                }
+                return answer;
+            }
+        }
     }
 }
